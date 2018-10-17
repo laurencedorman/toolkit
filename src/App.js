61b4c2596b76f40f669d9e08338c05e10cfeacc4
Maxime Modolo <maxime.modolo@manomano.fr>
2018-10-17 11:32:01 +0200

    Update Typo style and add Variables scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
+import './typo.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>Core React Project</h1>
+        <h2>Title</h2>
+        <h3>Title</h3>
+        <h4>Title</h4>
+        <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
+        <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
+        <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
       </div>
     );
   }
