2eeee9b0d2baea25ada4abfe1eead976c9c1d1da
Maxime Modolo <maxime.modolo@manomano.fr>
2018-10-22 14:40:57 +0200

    Add function to check if tooltip is in window

--- src/components/Tooltip/TooltipPosition.jsx
+++ src/components/Tooltip/TooltipPosition.jsx
@@ -1,8 +1,35 @@
-const TooltipPosition = (ref, position) => {
-  const { top, left, width, right, height } = ref.getBoundingClientRect();
-  const space = 12;
+
+export const tooltipPositionCheck = (ref, position) => {
+  const { top, left, right, bottom } = ref.getBoundingClientRect();
+  const requiredSpace = 100;
+
+  if (window.innerWidth - right < requiredSpace) return 'forceLeft';
+  else if (left < requiredSpace) return 'forceRight';
+  else if (top < requiredSpace) return 'forceBottom';
+  else if (window.innerHeight - bottom < requiredSpace) return 'forceTop';
+  else if (position === 'top') return 'top';
+  else if (position === 'left') return 'left';
+  else if (position === 'right') return 'right';
+  else if (position === 'bottom') return 'bottom';
+
+  return null;
+};
+
+export const tooltipPosition = (ref, checkPosition) => {
+  const { top, left, right, width, height, bottom } = ref.getBoundingClientRect();
+  const space = 16;
   const lateralPositionWidth = 176;
   const bottomSpace = document.body.clientHeight - top - window.scrollY;
+  const sideSpace = document.body.clientWidth - left + window.scrollX;
+
+  /*console.log(`
+    top: ${top} \n
+    bottom: ${bottom} \n
+    left: ${left} \n
+    width: ${width} \n
+    right: ${right} \n
+    height: ${height}
+  `);*/
 
   const topStyle = {
     bottom: bottomSpace + space,
@@ -11,12 +38,12 @@ const TooltipPosition = (ref, position) => {
 
   const leftStyle = {
     bottom: bottomSpace - space,
-    left: left - lateralPositionWidth,
+    right: sideSpace + space,
   };
 
   const rightStyle = {
     bottom: bottomSpace - space,
-    right: right - lateralPositionWidth,
+    right: sideSpace - lateralPositionWidth - width - space,
   };
 
   const bottomStyle = {
@@ -24,13 +51,29 @@ const TooltipPosition = (ref, position) => {
     left: left + width / 2 + window.scrollX,
   };
 
-  switch(position) {
+  /*switch(position) {
     case 'top': return topStyle;
     case 'left': return leftStyle;
     case 'right': return rightStyle;
     case 'bottom': return bottomStyle;
     default: return topStyle
+  }*/
+
+  switch(checkPosition) {
+    case 'forceLeft':
+    case 'left':
+      return leftStyle;
+    case 'forceRight':
+    case 'right':
+      return rightStyle;
+    case 'forceBottom':
+    case 'bottom':
+      return bottomStyle;
+    case 'forceTop':
+    case 'top':
+      return topStyle;
+    default: return topStyle
   }
 };
 
-export default TooltipPosition;
+export default tooltipPosition;
