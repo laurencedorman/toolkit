de0c875c469b14c2460f14c142f39fed3b40ed0a
Maxime Modolo <maxime.modolo@manomano.fr>
2018-10-29 20:46:31 +0100

    Clean and preconfig before eject

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ class App extends Component {
     return (
       <div className="App">
        <h1>Core React Project</h1>
-        <h2>Title</h2>
-        <h3>Title</h3>
-        <h4>Title</h4>
-        <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
-        <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
-        <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. </p>
+        <p>jrijf</p>
       </div>
     );
   }
