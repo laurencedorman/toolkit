16229994b50c406908038f07ca85a359181cfd19
Maxime Modolo <maxime.modolo@manomano.fr>
2018-10-31 17:40:16 +0100

    rebase to eject config

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,12 +1,13 @@
+// @flow
 import React, { type Node } from 'react';
 import cn from 'classnames';
 import styles from './Tag.module.scss';
 
 const Tag = ({ className, children, disabled }: propTypes) => {
   const tagClass = cn(styles.tag, {
-		className,
-		[styles.disabled]: disabled,
-	});
+    className,
+    [styles.disabled]: disabled,
+  });
 
   return(
     <small className={tagClass}>
@@ -17,7 +18,7 @@ const Tag = ({ className, children, disabled }: propTypes) => {
 
 type propTypes = {
   className?: string,
-	children: string | Node,
+  children: string | Node,
   disabled: boolean,
 };
 
