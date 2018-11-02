e66a6aa405cbe71960522bd05c0c103bc1a99251
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-02 18:10:07 +0100

    Create button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,61 @@
+// @flow
+import React from 'react';
+import Icon from '../Icon';
+import cn from 'classnames';
+import styles from './Button.module.scss';
+
+
+/**
+ * @visibleName Button
+ */
+const Button = ({
+  children, size, theme, disabled, type, icon, reverse, onClick, dataQa }: propTypes) => {
+  const className = cn(
+    styles.manoolkitButton,
+    styles[size],
+    styles[theme],
+    {
+      [styles.icon]: icon,
+      [styles.reverse]: reverse,
+    }
+  );
+
+  return(
+    <button
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      className={className}
+      data-qa={dataQa}
+    >
+      {icon &&
+      <Icon name={icon} size="16" />}
+      {children}
+    </button>
+  );
+};
+
+type propTypes = {
+  children: string | Node,
+  theme?: 'default' | 'primary' | 'secondary',
+  size?: 'big' | 'default' | 'small' | 'xsmall',
+  type?: 'submit' | 'reset' | 'button' | 'menu',
+  icon?: string,
+  reverse?: boolean,
+  disabled?: boolean,
+  onClick?: () => void,
+  dataQa?: string,
+};
+
+Button.defaultProps = {
+  theme: 'default',
+  size: 'default',
+  type: 'button',
+  icon: '',
+  reverse: false,
+  disabled: false,
+  onClick: null,
+  dataQa: '',
+};
+
+export default Button;
