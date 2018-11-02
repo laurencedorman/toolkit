fda34dde544bc892756fee56f2ea8bee8e9db9f7
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-02 14:43:12 +0100

    update Hint with icon component

--- src/components/Hint/Hint.jsx
+++ src/components/Hint/Hint.jsx
@@ -1,13 +1,12 @@
 // @flow
 import React from 'react';
 import styles from './Hint.module.scss';
+import Icon from '../Icon';
 
-const Hint = ({ showAlert, transitionTime, theme, textAlert }: PropTypes) => (
+const Hint = ({ showAlert, transitionTime, theme, textAlert, icon, iconWidth }: PropTypes) => (
   <div className={`${styles.manoolkitAlert} ${styles[theme]}`}>
     <div className={styles.img}>
-      <svg width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
-        <path d="M16,32 C7.163444,32 0,24.836556 0,16 C0,7.163444 7.163444,0 16,0 C24.836556,0 32,7.163444 32,16 C32,24.836556 24.836556,32 16,32 Z M16,30 C23.7319865,30 30,23.7319865 30,16 C30,8.2680135 23.7319865,2 16,2 C8.2680135,2 2,8.2680135 2,16 C2,23.7319865 8.2680135,30 16,30 Z M16,21 C16.5522847,21 17,21.4477153 17,22 C17,22.5522847 16.5522847,23 16,23 C15.4477153,23 15,22.5522847 15,22 C15,21.4477153 15.4477153,21 16,21 Z M16,9 C16.5522847,9 17,9.44771525 17,10 L17,18 C17,18.5522847 16.5522847,19 16,19 C15.4477153,19 15,18.5522847 15,18 L15,10 C15,9.44771525 15.4477153,9 16,9 Z" />
-      </svg>
+      <Icon name={icon} size={iconWidth} />
     </div>
     <div>
       <p>{textAlert}</p>
@@ -18,10 +17,14 @@ const Hint = ({ showAlert, transitionTime, theme, textAlert }: PropTypes) => (
 type PropTypes = {
   theme?: Array<'default', 'light', 'danger', 'menthe'>,
   textAlert: string,
+  icon?: string,
+  iconWidth?: number,
 };
 
 Hint.defaultProps = {
   theme: 'default',
+  icon: 'alert-circle',
+  iconWidth: 32,
 };
 
 export default Hint;
