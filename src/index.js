c23393e9cc38d1b7b28c7dc9848b91d3de6990de
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-05 11:51:37 +0100

    Add missing index export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+export { default as Button } from './components/Button';
 export { default as Card } from './components/Card';
+export { default as Hint } from './components/Hint';
 export { default as Icon } from './components/Icon';
 export { default as Input } from './components/Input';
 export { default as InputCheck } from './components/InputCheck';
