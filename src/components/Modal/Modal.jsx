4778ad7fde71f2647624959c852697d891fdcc63
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-06 16:18:48 +0100

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 // @flow
-import React, { Fragment, PureComponent } from 'react';
-// import ReactDOM from 'react-dom';
+import React, { PureComponent } from 'react';
+import ReactDOM from 'react-dom';
 import { Transition } from 'react-transition-group';
 import Icon from '../Icon';
 import styles from './Modal.module.scss';
@@ -9,69 +9,55 @@ import styles from './Modal.module.scss';
  * @visibleName Modal
  */
 type propTypes = {
+  open?: boolean,
+  close?: () => void,
+  transitionTime?: number,
   children: string | Node,
-  content: string,
-  className: string,
 };
 
 export default class Modal extends PureComponent<propTypes> {
   static defaultProps = {
-    className: '',
+    open: false,
+    close: null,
+    transitionTime: 150,
   };
 
-  constructor(props) {
-    super(props);
-    this.state = { active: false }
-  }
-
-  handleKey = e => e.keyCode === 27 && this.setState({ active: false });
-  handleShow = () => this.setState({ active: true });
-  handleHide = () => this.setState({ active: false });
-
   componentDidMount() {
-    document.addEventListener("keydown", this.handleKey, false);
+    document.addEventListener('keydown',
+      e => e.key === 'Escape' && this.props.close(), false
+    );
   };
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKey, false);
-  }
-
-  renderModal = () => {
-    const { content } = this.props;
-    const { active } = this.state;
-
-    return (
-      <Transition in={active} timeout={300}>
-        {state => (
-          <div
-            className={`${styles.modal} ${styles[state]}`}
-            onClick={this.handleHide}
-            onKeyPress={this.handleKey}>
-            <div>
-              <Icon
-                name="close-circle"
-                size="26"
-                onClick={this.handleHide}
-                className={styles.icon} />
-              <p>{content}</p>
-            </div>
-          </div>
-        )}
-      </Transition>
-    )
+    document.removeEventListener('keydown', this.props.close(), false);
   };
 
   render() {
-    const { children, className } = this.props;
+    const { children, open, close, transitionTime } = this.props;
 
     return(
-      <Fragment>
-        <div
-          className={`${styles.trigger} ${className}`}
-          onClick={this.handleShow}
-        >{children}</div>
-        {this.renderModal()}
-      </Fragment>
+      <Transition
+        appear
+        in={open}
+        timeout={transitionTime}>
+        {state => {
+          console.log(state);
+          return(
+            <div
+              className={`${styles.modal} ${styles[state]}`}
+              onClick={close}>
+              <div>
+                <Icon
+                  name="close-circle"
+                  size="26"
+                  onClick={close}
+                  className={styles.icon} />
+                {children}
+              </div>
+            </div>
+          )
+        }}
+      </Transition>
     )
   }
 }
--- src/components/Modal/Modal.md
+++ src/components/Modal/Modal.md
@@ -1,12 +1,18 @@
 
-modal default :
-```js
-<div>
-<Modal
-  content="My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is."
->
-press on fire
-</Modal>
 
+
+modal with props trigger :
+```js
+initialState = {
+  show: false,
+};
+
+<div id="root">
+    <button onClick={() => setState({ show: true })}>open by controlled props</button>
+    <Modal
+      open={state.show}
+      close={() => setState({ show: false })}>
+      <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there.</p>
+    </Modal>
 </div>
 ```
