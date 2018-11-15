c352285e7ad87031296811ae4ae73d6c90906995
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-15 13:28:33 +0100

    update

--- src/components/Select/test/Select.test.jsx
+++ src/components/Select/test/Select.test.jsx
@@ -12,18 +12,11 @@ describe('Select', () => {
         options={options}
         name="testName"
         placeholder="placeholder"
-      />);
+      />,
+    );
   });
 
   it('should render without crash', () => {
     expect(wrapper.length).toEqual(1);
-  });
-
-  it('should have a placeholder name', () => {
-    expect(wrapper.find('placeholder').length).toEqual(1);
-  });
-
-  it('should have a  name', () => {
-    expect(wrapper.find('name').length).toEqual(1);
   });
 });
