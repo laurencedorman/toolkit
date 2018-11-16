fbadeba75907cfec37a0c8ee5d73675ab20f97f1
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-16 16:54:36 +0100

    Update modal with react spring

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,58 +1,96 @@
 // @flow
-import React from 'react';
-import { Transition } from 'react-transition-group';
-import { Icon, Portal } from 'components';
+import React, { Component } from 'react';
+import {
+  Transition, animated, interpolate, config,
+} from 'react-spring';
+import Icon from '../Icon';
+import Portal from '../Portal';
 import styles from './Modal.module.scss';
 
 /**
  * @visibleName Modal
  */
-const Modal = ({
-  on, toggle, transitionTime, children,
-}: propTypes) => (
-  <Portal>
-    <Transition
-      in={on}
-      mountOnEnter
-      unmountOnExit
-      timeout={transitionTime}
-    >
-      {state => (
-        /* eslint-disable */
-        <div
-          onClick={toggle}
-          className={`${styles.modal} ${styles[state]}`}
-          role="Dialog"
-        >
-          <div
-            className={`${styles.content} ${styles[state]}`}
-            onClick={e => e.stopPropagation()}
-            role="Contentinfo"
-          >
-            <Icon
-              name="close-circle"
-              size="26"
-              onClick={toggle}
-              className={styles.icon}
-            />
-            {children}
-          </div>
-        </div>
-        /* eslint-disable */
-      )}
-    </Transition>
-  </Portal>
-);
-
 type propTypes = {
   on: boolean,
   toggle: () => void,
-  transitionTime?: number,
   children: string | Node,
 };
+/* eslint-disable */
+export default class Modal extends Component<propTypes>{
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKey, false);
+  }
 
-Modal.defaultProps = {
-  transitionTime: 300,
-};
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKey, false);
+  }
 
-export default Modal;
+  handleKey = e => {
+    const { on, toggle } = this.props;
+
+    const keys = {
+      Escape: () => {
+        e.preventDefault();
+        !on
+          ? e.stopPropagation()
+          : toggle();
+      },
+    };
+
+    keys[e.key]
+      && keys[e.key]()
+  };
+
+  render() {
+    const { on, toggle, children } = this.props;
+
+    return (
+      <Portal>
+        <Transition
+          native
+          config={config.stiff}
+          items={on}
+          from={{ o: 0.01, s: 0.6, y: '-200px' }}
+          enter={{ o: 1, s: 1, y: '0px' }}
+          leave={{ o: 0.01, s: 0.6, y: '200px' }}
+        >
+          {on => on
+            && (
+              ({ o, s, y}) => (
+                <animated.div
+                  onClick={toggle}
+                  className={styles.modal}
+                  role="Dialog"
+                  style={{
+                    opacity: o.interpolate(o => o)
+                  }}
+                >
+                  <animated.div
+                    className={styles.content}
+                    onClick={e => e.stopPropagation()}
+                    role="Contentinfo"
+                    style={{
+                      transform: interpolate(
+                        [s, y],
+                        (s, y) => `scale(${s}) translate3d(0, ${y}, 0)`
+                      )
+                    }}
+                  >
+                    <Icon
+                      name="close-circle"
+                      size="26"
+                      onClick={toggle}
+                      className={styles.icon}
+                    />
+                    <div>{children}</div>
+                  </animated.div>
+                </animated.div>
+              )
+            )
+          }
+        </Transition>
+      </Portal>
+    )
+  }
+}
+/* eslint-disable */
