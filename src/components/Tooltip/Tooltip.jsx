b92e34722bcd7b5b8458551d38733bc0e8e87065
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-16 17:44:51 +0100

    update tooltip with react spring

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,6 +1,8 @@
 // @flow
 import React, { Fragment, PureComponent } from 'react';
-// import { Transition, animated } from 'react-spring';
+import {
+  Transition, animated, interpolate, config,
+} from 'react-spring';
 import Portal from '../Portal';
 import { tooltipPosition } from './TooltipPosition';
 import styles from './Tooltip.module.scss';
@@ -27,24 +29,51 @@ export default class Tooltip extends PureComponent<propTypes> {
     if (this.transmitter.current) {
       addTooltipPosition = tooltipPosition(this.transmitter.current, position);
     }
-
+    /* eslint-disable */
     return (
       <Portal>
-        {display
-          && (
-            <div className={styles.tooltip} style={addTooltipPosition.style}>
-              <div
-                id="tooltip-content"
-                className={styles[addTooltipPosition.class]}
-              >
-                {renderArrow()}
-                {content}
-              </div>
-            </div>
-          )
-        }
+        <Transition
+          native
+          config={config.wobbly}
+          items={display}
+          from={{ o: 0.01, s: 0.6, y: '-20px' }}
+          enter={{ o: 1, s: 1, y: '0px' }}
+          leave={{ o: 0.01, s: 0.6, y: '20px' }}
+        >
+          {display => display
+            && (
+              ({ o, s, y}) => (
+                <div
+                  className={styles.tooltip}
+                  style={addTooltipPosition.style}
+                >
+                  <animated.div
+                    id="tooltip-content"
+                    className={styles[addTooltipPosition.class]}
+                  >
+                    <animated.div
+                      style={{
+                        opacity: o.interpolate(o => o),
+                        transform: interpolate(
+                          [s, y],
+                          (s, y) => `scale(${s}) translate3d(0, ${y}, 0)`
+                        )
+                      }}
+                    >
+                      {renderArrow()}
+                      <span className={styles.span}>
+                         {content}
+                      </span>
+                    </animated.div>
+                  </animated.div>
+                </div>
+              )
+            )
+          }
+        </Transition>
       </Portal>
     );
+    /* eslint-disable */
   };
 
   render() {
@@ -77,7 +106,7 @@ export default class Tooltip extends PureComponent<propTypes> {
 };
 
 const renderArrow = () =>
-  <div className={styles.svg}>
+  <animated.div className={styles.svg}>
     <svg xmlns="http://www.w3.org/2000/svg"
          viewBox="0 0 18.39 7.96"
          width="16"
@@ -85,4 +114,4 @@ const renderArrow = () =>
     >
       <path d="M18.39,0,11.31,7.08a3,3,0,0,1-4.23,0L0,0Z" />
     </svg>
-  </div>;
+  </animated.div>;
--- src/components/Tooltip/Tooltip.md
+++ src/components/Tooltip/Tooltip.md
@@ -1,4 +1,4 @@
 Tooltip: 
 
-    <p>This tooltip component is <Tooltip content="I'm in Portal">awesome</Tooltip> !</p>
+    <p>This is tooltip component and <Tooltip content="Awesome, i'm in Portal so no z-index and overflow conflict">hover me</Tooltip> to try it!</p>
    
