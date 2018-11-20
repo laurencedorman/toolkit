31c88d7c93a9459d23e1fdfa32066f45891040c9
Maxime Modolo <maxime.modolo@manomano.fr>
2018-11-20 16:08:19 +0100

    Add handle key to close modal with escape

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -16,6 +16,7 @@ type propTypes = {
   children: string | Node,
 };
 /* eslint-disable */
+<<<<<<< HEAD
 export default class Modal extends Component<propTypes>{
   componentDidMount() {
     document.addEventListener('keydown', this.handleKey, false);
@@ -24,6 +25,84 @@ export default class Modal extends Component<propTypes>{
   componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKey, false);
   }
+=======
+class Modal extends Component<propTypes>{
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKey);
+  }
+
+  handleKey = (e) => {
+    const { toggle } = this.props;
+
+    const keys = {
+      Escape: () => {
+        e.preventDefault();
+        toggle();
+        document.removeEventListener('keydown', this.handleKey, false);
+      },
+    };
+
+    if (keys[e.key]) { keys[e.key](); }
+  };
+
+  render() {
+    const { on, toggle, children } = this.props;
+
+    return (
+      <Portal>
+        <Transition
+          native
+          config={config.stiff}
+          items={on}
+          from={{ o: 0.01, s: 0.6, y: '-200px' }}
+          enter={{ o: 1, s: 1, y: '0px' }}
+          leave={{ o: 0.01, s: 0.6, y: '200px' }}
+        >
+          {on => on
+            && (
+              ({ o, s, y}) => (
+                <animated.div
+                  onClick={toggle}
+                  className={styles.modal}
+                  role="Dialog"
+                  style={{
+                    opacity: o.interpolate(o => o)
+                  }}
+                >
+                  <animated.div
+                    className={styles.content}
+                    onClick={e => e.stopPropagation()}
+                    role="Contentinfo"
+                    style={{
+                      transform: interpolate(
+                        [s, y],
+                        (s, y) => `scale(${s}) translate3d(0, ${y}, 0)`
+                      )
+                    }}
+                  >
+                    <Icon
+                      name="close-circle"
+                      size="26"
+                      onClick={toggle}
+                      className={styles.icon}
+                    />
+                    <div>{children}</div>
+                  </animated.div>
+                </animated.div>
+              )
+            )
+          }
+        </Transition>
+      </Portal>
+    )
+  }
+}
+/* eslint-disable */
+>>>>>>> Add handle key to close modal with escape
 
   handleKey = e => {
     const { on, toggle } = this.props;
