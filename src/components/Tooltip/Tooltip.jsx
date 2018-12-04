fe22dc6452bf1ec3fd37793e763e72f154ed0022
Maxime Modolo <maxime.modolo@manomano.fr>
2018-12-04 19:17:26 +0100

    revert options

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -48,8 +48,6 @@ export default class Tooltip extends PureComponent<propTypes> {
       <Portal>
         <Transition
           native
-          unique
-          reset
           config={{ tension: 250, friction: 20, mass: 0.2 }}
           items={display}
           from={{ o: 0, s: 0.6 }}
