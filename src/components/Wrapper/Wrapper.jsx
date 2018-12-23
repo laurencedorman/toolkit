4fbd2a636b8f8782ac63ee982c3fce9625288a0c
Maxime Modolo <maxime.modolo@manomano.fr>
2018-12-23 23:24:34 +0100

    Add Wrapper type to change HTML tag and use wrapper as Header or Footer or div by default

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import cn from 'classnames';
 import styles from './Wrapper.module.scss';
 
+/**
+ * @visibleName Wrapper/Header/Footer
+ */
 const Wrapper = ({
-  children, className, direction, style,
+  children, className, direction, type,
 }:propTypes) => {
   const classNames = cn(
     className,
@@ -14,28 +17,39 @@ const Wrapper = ({
     },
   );
 
-  return (
-    <div
-      className={classNames}
-      direction={direction}
-      style={style}
-    >
-      {children}
-    </div>
+  return React.cloneElement(
+    renderType(type, children),
+    {
+      className: classNames,
+      type,
+      direction,
+    },
+
   );
 };
+
+/* eslint-disable */
+const renderType = (type, children) => (
+  type === 'div'
+    ? <div>{children}</div>
+    : type === 'header'
+      ? <header>{children}</header>
+      : <footer>{children}</footer>
+);
+/* eslint-enable */
+
 
 type propTypes = {
   children: string | Node | () => void,
   className?: string,
   direction?: string,
-  style?: Object,
+  type?: 'div' | 'header' | 'footer',
 };
 
 Wrapper.defaultProps = {
   className: '',
   direction: 'column',
-  style: {},
+  type: 'div',
 };
 
 export default Wrapper;
--- src/components/Wrapper/Wrapper.md
+++ src/components/Wrapper/Wrapper.md
@@ -1,8 +1,21 @@
 ```js
 <Wrapper>
-  <h2>Simple Wrapper with display Flex</h2>
+  <h3>Simple Wrapper with display Flex and tag HTML type</h3>
   <ul>
-    <li>choose content direction with props "direction" (column by default)</li>
+    <li>choose <b>content direction</b> with props "<b>direction</b>" (column by default)</li>
+    <li>choose <b>tag type</b> of wrapper "<b>div || header || footer</b>" (div by default)</li>
   </ul>
+</Wrapper>
+```
+
+```js
+<Wrapper type="header">
+  <h3>Wrapper with <b>header</b> type</h3>
+</Wrapper>
+```
+
+```js
+<Wrapper type="footer">
+  <h3>Wrapper with <b>footer</b> type</h3>
 </Wrapper>
 ```
