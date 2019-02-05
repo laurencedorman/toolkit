914c09b0d20830896e66f9460fb5c44cbbf20836
Maxime Modolo <maxime.modolo@manomano.fr>
2019-02-05 13:42:39 +0100

    Add ignore file on styleguidist

--- styleguide.config.js
+++ styleguide.config.js
@@ -90,6 +90,7 @@ module.exports = {
         'src/components/GetMeasure/*.jsx',
         'src/components/Portal/*.jsx',
         'src/components/StepControler/*.jsx',
+        'src/components/Tabs/TabBody.jsx',
         'src/components/Toggle/*.jsx',
         'src/components/ToggleDown/*.jsx',
         'src/components/Input/InputLight.jsx',
