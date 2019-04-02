34bfb714f7fd16a03fd6ddc2dfb8ebbb94e20fb2
Pierre TROLLE <pierre.trolle@manomano.com>
2019-04-02 10:54:19 +0200

    add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const mmConfig = require('@manomano/jest-config');
+
+module.exports = Object.assign({}, mmConfig, {
+  collectCoverage: true,
+  coverageReporters: ['text'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+});
